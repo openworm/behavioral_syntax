dad18afc80bac05bb93a0e595fd59f45541bb0fd
Aidan Rocke <aidan.rocke@gmail.com>
2015-06-16 11:47:26 +0200

    Create vectorCor.py

---
+++ utilities/vectorCor.py
@@ -0,0 +1,19 @@
+# Input
+#   mat1,2 - Two matrices with the same number of columns.
+# 
+# Output
+#   Rmat   - A matrix of correlation coefficients.  Rmat(i, j) is the
+#            correlation coefficient between the ith row of mat1 and the
+#            jth row of mat2.  Rmat has dimensions size(mat1, 1) by
+#            size(mat2, 1).
+
+import numpy as np
+
+def vectorCor(mat1,mat2):
+    N = np.shape(mat1)[0]
+    M = np.shape(mat2)[0]
+    for i in range(N):
+        for j in range(N):
+            Rmat[i][j] = np.corrcoef(mat1[i],mat2[j])
+    
+    return Rmat
