aa309907b2e07d960e1b3f31572db8c828648881
Aidan Rocke <aidan.rocke@gmail.com>
2015-06-16 16:24:20 +0200

    Update kmeans.py

--- utilities/kmeans.py
+++ utilities/kmeans.py
@@ -1,34 +1,15 @@
+#I am not using the parameter k but I don't think it's necessary. 
+
 import numpy as np
+from sklearn import cluster
+
+def kmeans(X):
+    clusters = np.shape(X)[1]
+    kmeans = cluster.KMeans(n_clusters=clusters, init='k-means++')
+    return  kmeans.fit_predict(X)
+
+
+X = np.random.rand(2,3)
+
+kmeans(X)
  
-def cluster_points(X, mu):
-    clusters  = {}
-    for x in X:
-        bestmukey = min([(i[0], np.linalg.norm(x-mu[i[0]])) \
-                    for i in enumerate(mu)], key=lambda t:t[1])[0]
-        try:
-            clusters[bestmukey].append(x)
-        except KeyError:
-            clusters[bestmukey] = [x]
-    return clusters
- 
-def reevaluate_centers(mu, clusters):
-    newmu = []
-    keys = sorted(clusters.keys())
-    for k in keys:
-        newmu.append(np.mean(clusters[k], axis = 0))
-    return newmu
- 
-def has_converged(mu, oldmu):
-    return (set([tuple(a) for a in mu]) == set([tuple(a) for a in oldmu])
- 
-def find_centers(X, K):
-    # Initialize to K random centers
-    oldmu = random.sample(X, K)
-    mu = random.sample(X, K)
-    while not has_converged(mu, oldmu):
-        oldmu = mu
-        # Assign all points in X to clusters
-        clusters = cluster_points(X, mu)
-        # Reevaluate centers
-        mu = reevaluate_centers(oldmu, clusters)
-    return(mu, clusters)
