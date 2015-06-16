6b4e1af782954298a6d06c16243c71ca64e041b3
Aidan Rocke <aidan.rocke@gmail.com>
2015-06-16 14:25:57 +0200

    doing it right

--- utilities/vectorCor.py
+++ utilities/vectorCor.py
@@ -7,14 +7,15 @@
 #            jth row of mat2.  Rmat has dimensions size(mat1, 1) by
 #            size(mat2, 1).
 
+
 import numpy as np
 
 def vectorCor(mat1,mat2):
     N = np.shape(mat1)[0]
     M = np.shape(mat2)[0]
-    Rmat = [[0 for i in range(N)] for j in range(M)]
+    Rmat = [[0 for i in range(M)] for j in range(N)]
     for i in range(N):
-        for j in range(N):
-            Rmat[i][j] = np.corrcoef(mat1[i],mat2[j])
+        for j in range(M):
+            Rmat[i][j] = np.corrcoef(mat1[i],mat2[j])[0][1]
     
     return Rmat
