451f5493e3593b9b20ab6f8c345b56b516a0c5a4
Aidan Rocke <aidan.rocke@gmail.com>
2015-06-23 14:15:18 +0100

    use list instead of cell

--- utilities/mat2rowcell.py
+++ utilities/mat2rowcell.py
@@ -2,9 +2,14 @@ import scipy.io as sio
 
 'using the idea described here: http://www.codedisqus.com/0NzVejPWWq/creating-matlab-cell-arrays-in-python.html'
 
+#matrix is assumed to be a multi-dimensional matlab array
+#example input: matrix = sio.loadmat('matrix.mat')
+
+#instead of converting to a Matlab cell array I have decided to convert to a Python list
+#as Python lists can hold any object.
+
 def mat2rowcell(matrix):
-  N = length(matrix)
   matrix = np.array(matrix)
-  cell = np.empty(matrix.shape, dtype=object)
-  for i in range(N):
-    cell[0,i,0] = np.array([matrix(i)])
+  N = length(matrix)
+  list = matrix.tolist()
+  return list
