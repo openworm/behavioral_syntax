805cd839953ab5acf40161925cc0a0e5d28922ad
Aidan Rocke <aidan.rocke@gmail.com>
2015-07-05 01:28:44 +0100

    cleaner code

--- utilities/loading_data.py
+++ utilities/loading_data.py
@@ -1,15 +1,21 @@
+import angle, scipy, h5py
+
 f = h5py.File('C:/Users/ltopuser/behavioral_syntax/utilities/data.mat')
 
+#getting the right cell array:
 l1 = f.get('worm')
 
 l2 = l1.get('posture')
 
 l3 = l2.get('skeleton')
 
+#getting the x and y coordinates:
 X = l3.get('x')
 
 Y = l3.get('y')
 
 angleArray, meanAngles = angle(X,Y)
 
-scipy.io.savemat('angles.mat', dict(x=angleArray))
+#save data
+scipy.io.savemat('angleArray.mat', dict(x=angleArray))
+scipy.io.savemat('meanAngles.mat', dict(y=meanAngles))
