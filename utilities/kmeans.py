1423115010ee5b37e21b68c5f1872c88d1524063
Aidan Rocke <aidan.rocke@gmail.com>
2015-07-07 20:34:16 +0100

    80%
    All I have to do is rotate the angles if they are facing the LHS.

--- utilities/kmeans.py
+++ utilities/kmeans.py
@@ -1,43 +1,12 @@
-import numpy as np
-#from sklearn import cluster
+from sklearn.cluster import KMeans
 
+def kmeans(angle_data):
+#here I'm assuming that all the angle arrays have been rotated so that 
+#they are on directed to the right. 
+    k_means = KMeans(init='k-means++', n_clusters=90, n_init=20)
 
-def initialize(X, K):
-    C = [X[0]]
-    for k in range(1, K):
-        D2 = scipy.array([min([scipy.inner(c-x,c-x) for c in C]) for x in X])
-        probs = D2/D2.sum()
-        cumprobs = probs.cumsum()
-        r = scipy.rand()
-        for j,p in enumerate(cumprobs):
-            if r < p:
-                i = j
-                break
-        C.append(X[i])
-    return C
-
-def calcJ(X, centers):
-    diffsq = (centers[:,np.newaxis,:]-X)**2
-    return np.sum(np.min(np.sum(diffsq,axis=2),axis=0))
+    k_means.fit(angle_data)
     
-def kmeans(X,k,n=100):
-    #initialize centers and list J to track performance metric
-    centers = initalize(X,k)
-    J = []
+    postures = k_means.cluster_centers_
     
-    #repeat n times
-    for iteration in range(n):
-        #zhich center is each sample closest to?
-        sqdistances = np.sum((centers[:,np.newaxis,:]-X)**2,axis=2)
-        closest = np.argmin(sqdistances, axis=0)
-        
-        #calculate J and append to list J
-        J.append(calcJ(X,centers))
-        
-        #update cluster centers
-        for i in range(k):
-            centers [i,:] = X[closest=i,:].mean(axis=0)
-            
-        #calculate J a final time and return results
-        J.append(calcJ(data,centers))
-        return centers, J, closest
+    return postures
