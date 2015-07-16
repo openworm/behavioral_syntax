f2999446162555145c306a24ee53cfedda0ca445
Aidan Rocke <aidan.rocke@gmail.com>
2015-07-16 21:35:58 +0800

    convert to function

--- utilities/loading_data.py
+++ utilities/loading_data.py
@@ -1,25 +1,29 @@
 import angle, scipy, h5py
 
 #ex. filepath = 'C:/Users/ltopuser/behavioral_syntax/utilities/data.mat'
-f = h5py.File(filepath)
 
-#getting the right cell array:
-l1 = f.get('worm')
-
-l2 = l1.get('posture')
-
-l3 = l2.get('skeleton')
-
-#getting the x and y coordinates:
-X = l3.get('x')
-
-Y = l3.get('y')
-
-angleArray, meanAngles = angle(X,Y)
-
-#save data
-scipy.io.savemat('angleArray.mat', dict(x=angleArray))
-scipy.io.savemat('meanAngles.mat', dict(y=meanAngles))
+def loading_data(filepath):
+  f = h5py.File(filepath)
+  
+  #getting the right cell array:
+  l1 = f.get('worm')
+  
+  l2 = l1.get('posture')
+  
+  l3 = l2.get('skeleton')
+  
+  #getting the x and y coordinates:
+  X = l3.get('x')
+  
+  Y = l3.get('y')
+  
+  angleArray, meanAngles = angle(X,Y)
+  
+  #save data
+  scipy.io.savemat('angleArray.mat', dict(x=angleArray))
+  scipy.io.savemat('meanAngles.mat', dict(y=meanAngles))
+  
+  return angleArray, meanAngles
 
 
 #loading data:
