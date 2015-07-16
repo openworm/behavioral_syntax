c77bef70afd55db6e8fd6c46f71013daee66e146
Aidan Rocke <aidan.rocke@gmail.com>
2015-07-16 21:52:51 +0800

    100%

--- utilities/angle.py
+++ utilities/angle.py
@@ -1,63 +1,28 @@
-import numpy as np
-import math
-import pandas as pd
+from angle import angle
 
-def angle(x, y):
+import scipy, h5py
 
-#given a series of x and y coordinates over time, calculates the angle
-#between each vector making up the skeleton and the x-axis.  The mean angle
-#is subtracted from each frame so that the average orientation is always
-#the same in the output angleArray.
+#ex. filepath = 'C:/Users/ltopuser/behavioral_syntax/utilities/data.mat'
 
-
-
-
-    [numFrames, lengthX] = np.shape(x)
-    
-    # initialize arrays
-    angleArray = np.zeros([numFrames, lengthX-1])
-    meanAngles = np.zeros([numFrames,1])
-    
-    
-    for i in range(numFrames):
-        
-        # calculate the x and y differences
-        dX = np.diff(x[i], n=1, axis=0)
-        dY = np.diff(y[i], n=1, axis=0)
-        
-        # calculate tangent angles.  atan2 uses angles from -pi to pi instead...
-        # of atan which uses the range -pi/2 to pi/2.
-        angles = np.arctan2(dY, dX)
-        
-        # need to deal with cases where angle changes discontinuously from -pi
-        # to pi and pi to -pi.  In these cases, subtract 2pi and add 2pi
-        # respectively to all remaining points.  This effectively extends the
-        # range outside the -pi to pi range.  Everything is re-centred later
-        # when we subtract off the mean.
-        
-        # find discontinuities
-        positiveJumps = np.array((np.diff(angles, n=1, axis=0) > 5)) + 1
-        negativeJumps = np.array((np.diff(angles, n=1, axis=0) < -5)) + 1
-        
-        # subtract 2pi from remainging data after positive jumps
-        for j in range(len(positiveJumps)):
-            angles[positiveJumps[j]:] = angles[positiveJumps[j]:] - 2*math.pi
-        
-        # add 2pi to remaining data after negative jumps
-        for j in range(len(negativeJumps)):
-            angles[negativeJumps[j]:] = angles[negativeJumps[j]:] + 2*math.pi
-        
-        # rotate skeleton angles so that mean orientation is zero
-        meanAngle = np.mean(angles)
-        meanAngles[i] = meanAngle
-        angles -= meanAngle
-        
-        # append to angle array
-        
-        angleArray[i] = pd.Series(angles).interpolate().values
-        
-        
-    #interpolate over NaNs in meanAngles:
-    meanAngles = meanAngles.flatten()
-    meanAngles = pd.Series(meanAngles).interpolate().values
-    return angleArray, meanAngles
+def loading_data(filepath):
+  f = h5py.File(filepath)
+  
+  #getting the right cell array:
+  l1 = f.get('worm')
+  
+  l2 = l1.get('posture')
+  
+  l3 = l2.get('skeleton')
+  
+  #getting the x and y coordinates:
+  X = l3.get('x')
+  
+  Y = l3.get('y')
+  
+  angleArray, meanAngles = angle(X,Y)
+  
+  #save data
+  scipy.io.savemat('angleArray.mat', dict(x=angleArray))
+  scipy.io.savemat('meanAngles.mat', dict(y=meanAngles))
+  
+  return angleArray, meanAngles
