65f845c6a18ddaa205171a44ea29c81537ff7136
Aidan Rocke <aidan.rocke@gmail.com>
2015-08-14 13:00:27 +0800

    Update and rename simple_compression.py to time_warp.py

--- compression/time_warp.py
+++ compression/time_warp.py
@@ -5,7 +5,7 @@ Created on Sun Jul 19 07:28:51 2015
 @author: Dell
 """
 
-def Simple_compression(xs):
+def time_warp(xs):
     """ Return a new list in which all adjacent
         duplicates from xs have been removed.
     """
