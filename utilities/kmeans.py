e35c776973789346862a7dcf14a30b1ee1291a91
Aidan Rocke <aidan.rocke@gmail.com>
2015-08-18 14:10:59 +0800

    100%

--- utilities/kmeans.py
+++ utilities/kmeans.py
@@ -1,12 +1,40 @@
-from sklearn.cluster import KMeans
+from scipy import io
+import numpy as np
+from matplotlib import pyplot as plt
+from sklearn import manifold, cluster
 
-def kmeans(angle_data):
-#here I'm assuming that all the angle arrays have been rotated so that 
-#they are on directed to the right. 
-    k_means = KMeans(init='k-means++', n_clusters=90, n_init=20)
+directory = '/Users/cyrilrocke/Documents/c_elegans/data/'
+angles = np.load(directory+'/test1/features/angles10.npy')
 
-    k_means.fit(angle_data)
+#mean_r_sq finds the template postures using kmeans++ and returns the 
+#average variance explained(r_squared), the templates, and labels.
+
+#decision_rule calls the mean_r_sq as many times as required to satisfy
+#the criterion that the r_squared gain is negligible. 
+
+angle_0 = angles[0]
+
+def mean_r_sq(k):
+    kmeans = cluster.KMeans(init='k-means++',n_clusters=k,max_iter=1000)
+    kmeans.fit(angle_0)
     
-    postures = k_means.cluster_centers_
+    labels = kmeans.labels_
+    centroids = kmeans.cluster_centers_
     
-    return postures
+    #compute average variance explained:
+    total_var = []
+    for i in range(len(centroids)):
+        total_var+= [np.corrcoef(centroids[i],j)[0][1]**2 for j in angle_0[np.where(labels == i)]]
+    
+    return [np.mean(total_var), centroids, labels]
+
+
+# start = minimum cluster size
+# inc = increase in cluster size k
+# delta = R_squared gain that is considered negligible
+def decision_rule(prev,inc,delta):
+    
+    while mean_r_sq(prev+inc)[0]-mean_r_sq(prev)[0] > delta:
+        prev = prev+inc
+        output = mean_r_sq(prev)
+    return output
