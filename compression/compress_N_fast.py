686ff712ba67046cd8d0c98d16fa02b1ca54f760
Aidan Rocke <aidan.rocke@gmail.com>
2015-09-02 23:40:43 +0800

    90%

--- compression/compress_N_fast.py
+++ compression/compress_N_fast.py
@@ -4,7 +4,6 @@ Created on Tue Sep  1 23:01:49 2015
 
 @author: macbook
 """
-
 import numpy as np
 
 def compressSequenceNFast(dataVec, newStart, nMax):
@@ -18,7 +17,7 @@ def compressSequenceNFast(dataVec, newStart, nMax):
 # Proceedings of the IEEE 88:1745.
 #
 # Input
-#   dataVec  - a row vector of numbers to be compressed
+#   dataVec  - a list of posture sequences to be compressed
 #   newStart - this is the number that will be used to label the first new
 #              rule in the grammar.  It must be greater than the maximum
 #              value in dataVec.  If empty, then max(dataVec) + 1 is used.
@@ -38,7 +37,7 @@ def compressSequenceNFast(dataVec, newStart, nMax):
 
     # check dataVec
     if len(np.shape(posture_seq)) > 1:
-        print('dataVec must be a row vector.')
+        raise ValueError('dataVec must be a row vector.')
     
     # define newStart if left empty
     if newStart == 0:
@@ -46,10 +45,10 @@ def compressSequenceNFast(dataVec, newStart, nMax):
     
     # check that newStart is large enough
     if newStart <= max(dataVec):
-        print('newStart must be greater than max(dataVec).')
+        raise ValueError('newStart must be greater than max(dataVec).')
 
     # initialise grammar
-    grammar = list(np.zeros(2))
+    grammar = [[0 for x in range(2)] for x in range(len(sequence))]
     
     # initialise compVec and make a suffix array
     compVec = dataVec
@@ -60,46 +59,39 @@ def compressSequenceNFast(dataVec, newStart, nMax):
     newInd = newStart
     i = 1
     while len(sequence) > 0:
-    # find the most compressive sequence in dataVec
+        # find the most compressive sequence in dataVec
         [sequence, locations, savings] = compressiveNFast(compVec, nMax)
-    
-    # update the total savings (i.e. compression)
-    totSavings = totSavings + savings
-    
-    # add the rule to grammar
-    grammar{ii, 1} = newInd
-    grammar{ii, 2} = sequence
-    i+=1
-    # disp(ii);
-    
-    # make the replacements.  Note: strrep does not work here.  For example
-    # if sequence is [44 68 44] and compVec has a subsequence that is 
-    # [44 68 44 68 44 68 44 448], strrep will give [68 480 480 480 448]
-    # which is wrong.
-    for jj = 1:numel(locations)
-        compVec(locations(jj):locations(jj) + numel(sequence) - 1) = ...
-            [newInd NaN(1, numel(sequence) - 1)];
-    end
-    compVec(isnan(compVec)) = [];
-    
-    newInd = newInd + 1;
-    
-    # check that compressed lengths, savings, and grammar size are
-    # consistent
-    if ~isempty(sequence) # on last iteration last grammar entry is empty
-        if length(compVec) + totSavings + size(grammar, 1) + ...
-                sum(cellfun(@length, grammar(:, 2))) ~= length(dataVec)
-            error(['Calculated savings not consistent with original' ...
-                ' and compressed lengths and grammar size.'])
-        end
-    else
-        if length(compVec) + totSavings + size(grammar, 1) - 1 + ...
-                sum(cellfun(@length, grammar(:, 2))) ~= length(dataVec)
-            error(['Calculated savings not consistent with original' ...
-                ' and compressed lengths and grammar size.'])
-        end
-    end
-    end
-    
-    # remove the last (empty) entry of the grammar
-    grammar = grammar(1:end-1, :);
+        
+        # update the total savings (i.e. compression)
+        totSavings = totSavings + savings
+        
+        # add the rule to grammar
+        grammar[i][0] = newInd
+        grammar[i][1] = sequence
+        i+=1
+        # disp(ii)
+        
+        # make the replacements.  Note: strrep does not work here.  For example
+        # if sequence is [44 68 44] and compVec has a subsequence that is 
+        # [44 68 44 68 44 68 44 448], strrep will give [68 480 480 480 448]
+        # which is wrong.
+        for j in range(len(locations)):
+            compVec[locations[j]:locations[j] + len(sequence) - 1] = [newInd]+[np.nan]*(len(sequence)-1)
+        
+        while compVec.count(np.nan) > 0:
+            compVec.remove(np.nan)
+        
+        newInd += 1
+        
+        # check that compressed lengths, savings, and grammar size are
+        # consistent
+        if len(sequence) > 0: # on last iteration last grammar entry is empty
+            if len(compVec) + totSavings + len(grammar) + sum(cellfun(@length, grammar(:, 2))) ~= length(dataVec)
+                raise ValueError(['Calculated savings not consistent with original and compressed lengths and grammar size.'])
+        else
+            if length(compVec) + totSavings + (len(grammar)-1) +
+                    sum(cellfun(@length, grammar(:, 2))) ~= length(dataVec)
+                ValueError(['Calculated savings not consistent with original and compressed lengths and grammar size.'])
+        
+        # remove the last (empty) entry of the grammar
+        grammar = grammar(1:end-1, :);
