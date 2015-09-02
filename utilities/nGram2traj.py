47f8167d6495a4287c853a51699002bdddff0fff
Aidan Rocke <aidan.rocke@gmail.com>
2015-09-03 00:02:52 +0800

    Update nGram2traj.py

--- utilities/nGram2traj.py
+++ utilities/nGram2traj.py
@@ -1,4 +1,8 @@
-import numpy as np
+#from behavioral_syntax.compression.n_grams import n_grams
+
+#can I import data from a folder?
+
+#import numpy as np
 
 def nGram2traj(postures, nGrams):
 
@@ -8,17 +12,13 @@ def nGram2traj(postures, nGrams):
     #   nGrams  - a len(dataVec)-(n+1) by n array containing all the n-grams in dataVec  
     
     
+    n, m = len(nGrams), len(nGrams[0].split(' '))
     
+    angle_trajectories = [[0 for x in range(m)] for x in range(n)]
     
-    # get the number of points in each posture
-    nPts = np.shape(postures)[1]
+    for i in range(n):
+        l1 = nGrams[i].split(' ')
+        l2 = [int(i) for i in l1]
+        angle_trajectories[i] = [postures[l2[0]],postures[l2[1]],postures[l2[2]]]
     
-    # convert the n-grams to trajectories
-    val1 = np.shape(nGrams)[0]
-    val2 = nPts * np.shape(nGrams)[1]
-    trajectoryMat = np.array([[np.NaN]*val2 for i in range(val1)])
-    for j in range(val1):
-        for k in range(val2):
-            trajectoryMat[(k-1)*nPts+1:k*nPts][:,j] = postures[nGrams[j][k]]
-    
-    return trajectoryMat 
+    return angle_trajectories
