e68bd0feaa39542ed033be83861581ebf1810f5f
Aidan Rocke <aidan.rocke@gmail.com>
2015-09-21 18:01:32 +0100

    added_posture_visualization

--- plotting/vis_functions.py
+++ plotting/vis_functions.py
@@ -9,9 +9,42 @@ import matplotlib.pyplot as plt
 from math import sqrt
 from scipy.stats import itemfreq
 import numpy as np
+from scipy import io
 #for bokeh charts:
 from bokeh.plotting import show, output_file
 from bokeh.charts import Bar
+
+#visualizing postures is probably one of the most important tasks:
+g = io.loadmat('/Users/cyrilrocke/Documents/c_elegans/data/postures')
+postures = g.get('postures')
+plt.style.use('ggplot')
+
+def N_postures(sequence,color,title):
+    sequence = list(sequence)
+    mean_angles = np.zeros(90)
+    
+    for i in sequence:
+        mean_angles[i] = np.mean(postures[:,i])
+        
+    X,Y = MA2skel(postures.T,mean_angles,1)
+    
+    N = len(sequence)
+    if N>6:
+        n = round(sqrt(N))+1
+        fig, axes = plt.subplots(ncols=n, nrows=n)
+        fig.set_size_inches(n, n)
+    else:
+        fig, axes = plt.subplots(ncols=N, nrows=1)
+        fig.set_size_inches(N, round(N/2))
+    fig.suptitle(title, fontsize=14, fontweight='bold')
+    
+    axes = axes.ravel()
+    j = 0
+    for j in range(len(sequence)):
+        axes[j].plot(X[sequence[j]],Y[sequence[j]],color=color,lw=5)
+        axes[j].set_title(str(j),size='medium',weight='bold',color='steelblue',backgroundcolor=(1,  0.85490196,  0.7254902))
+        axes[j].axis('off')
+        j+=1
 
 def bokeh_bars(liszt,name):
     
