4d995b71bfa5ae454a64c8de3a12f527defd8e79
Aidan Rocke <aidan.rocke@gmail.com>
2015-11-02 14:10:53 +0000

    Update test_tw.py

--- tests/test_tw.py
+++ tests/test_tw.py
@@ -20,4 +20,4 @@ if __name__ == '__main__':
     doctest.testmod()'''
 
 def test_answer():
-    assert time_warp('1 2 3 4 5 5 2 3 3'.split(' ')) == 5
+    assert time_warp('1 2 3 4 5 5 2 3 3'.split(' ')) == ['1', '2', '3', '4', '5', '2', '3']
