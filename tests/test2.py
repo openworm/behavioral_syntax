0e35ed275370455216797c3a4c86cb590e534ee0
Aidan Rocke <aidan.rocke@gmail.com>
2015-11-02 14:20:04 +0000

    Update test2.py

--- tests/test2.py
+++ tests/test2.py
@@ -3,4 +3,4 @@ import behavioral_syntax as b_s
 from behavioral_syntax.compression import time_warp as tw
 
 def test_answer():
-    assert tw.time_warp('1 2 3 4 5 5 2 3 3'.split(' ')) == ['1', '2', '3', '4', '5', '2', '3'
+    assert tw.time_warp('1 2 3 4 5 5 2 3 3'.split(' ')) == ['1', '2', '3', '4', '5', '2', '3']
