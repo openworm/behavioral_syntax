a1fce0d460caebbd0ff51b132bfbeb57208553af
Aidan Rocke <aidan.rocke@gmail.com>
2015-11-04 18:29:34 +0000

    Update loading_data.py

--- utilities/loading_data.py
+++ utilities/loading_data.py
@@ -1,8 +1,8 @@
 from angle import angle
-
+import numpy as np
 import scipy, h5py
 
-#ex. filepath = 'C:/Users/ltopuser/behavioral_syntax/utilities/data.mat'
+#filepath = 'C:/Users/ltopuser/behavioral_syntax/utilities/data.mat'
 
 def loading_data(filepath):
   f = h5py.File(filepath)
@@ -26,13 +26,31 @@ def loading_data(filepath):
   scipy.io.savemat('meanAngles.mat', dict(y=meanAngles))
   
   return angleArray, meanAngles
+  
+def dict_generator(filepath, pre=None):
+    pre = pre[:] if pre else []
+    if isinstance(indict, dict):
+        for key, value in indict.items():
+            if isinstance(value, dict):
+                for d in dict_generator(value, [key] + pre):
+                    yield d
+            elif isinstance(value, list) or isinstance(value, tuple):
+                for v in value:
+                    for d in dict_generator(v, [key] + pre):
+                        yield d
+            else:
+                yield pre + [key, value]
+    else:
+        yield indict
+        
+z = sum([i for i in dict_generator(filepath)],[])
 
-
-#loading data:
-#angles = sio.loadmat('C:/Users/ltopuser/behavioral_syntax/data/angles.mat')
-#array = angles.get('x')
-
-
-#loading data:
-#angles = sio.loadmat('C:/Users/ltopuser/behavioral_syntax/data/angles.mat')
-#array = angles.get('x')
+i = 0
+if np.shape(z[i])==():
+    i+=1
+    
+elif len(np.shape(z[i]))==2 and max(np.shape(z[i]))> 1000 and min(np.shape(z[i])) == 48:
+    postures = z[i]
+    
+else:
+    i+=1
