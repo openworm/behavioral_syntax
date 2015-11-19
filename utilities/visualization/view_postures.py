b5a23b62b2a83f113331ac38f2606ecfef59122e
Aidan Rocke <aidanrocke@gmail.com>
2015-11-19 16:47:57 +0000

    all

--- utilities/visualization/view_postures.py
+++ utilities/visualization/view_postures.py
@@ -1,27 +1,31 @@
 from scipy import io
-from angle2skel2 import angle2skel
+from behavioral_syntax.utilities.angle_and_skel import MA2skel
 import numpy as np
 from matplotlib import pyplot as plt
 
-g = io.loadmat('/Users/cyrilrocke/Documents/c_elegans/postures')
+plt.style.use('ggplot')
 
-postures = g.get('postures')
+#g = io.loadmat('/Users/cyrilrocke/Documents/c_elegans/data/postures')
+
+G = io.loadmat('/Users/cyrilrocke/behavioral_syntax/data/postures')
+
+postures = G.get('postures')
 
 mean_angles = np.zeros(90)
 
 for i in range(90):
     mean_angles[i] = np.mean(postures[:,i])
     
-X,Y = angle2skel(postures.T,mean_angles,1)
-
-plt.style.use('ggplot')
+X,Y = MA2skel(postures.T,mean_angles,1)
 
 fig, axes = plt.subplots(ncols=10, nrows=9)
 fig.set_size_inches(10, 9)
 
+#worm head is colored green:
 j = 0
 for ax in axes.ravel():
-    ax.plot(X[j],Y[j],lw=3)
+    ax.plot(X[j][0:45],Y[j][0:45],lw=3)
+    ax.plot(X[j][44:48],Y[j][44:48],lw=3,color='green')
     ax.set_title(str(j),size='medium',weight='bold',color='steelblue',backgroundcolor=(1,  0.85490196,  0.7254902))
     ax.axis('off')
     j+=1
