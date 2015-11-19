6bcfdc60cb5a19f4482c8529f5151ddcfccbcf5b
Aidan Rocke <aidanrocke@gmail.com>
2015-11-19 18:16:00 +0000

    postures

--- discrete_models/discretization/posture_sequence.py
+++ discrete_models/discretization/posture_sequence.py
@@ -1,9 +1,5 @@
 import numpy as np
-import os
 from scipy import io
-import h5py
-
-from behavioral_syntax.utilities.angle_and_skel import angle
 
 from behavioral_syntax.utilities.loading_data import loading_data
 
@@ -19,62 +15,42 @@ postures = g.get('postures')
 all_postures = []
 
 
-def posture_seq(directory,postures,start,finish):
+def posture_seq(directory,postures,sampling_fraction):
     #I may not always want to be limited to 90 postures. 
     num_postures = len(postures)
     
-    i = start
-    while i < finish :
-        #fetch skeletons: 
-        if files[i].endswith('.mat'):
-            #get skeleton data:
-            '''
-            f = h5py.File(directory+files[i])
-            #getting the right cell array:
-            for i in ['worm','posture','skeleton']:
-                f = f.get(i)'''
-                
-            data = io.loadmat(directory+files[i])
-            X = data['worm']['posture'][0][0][0]['skeleton'][0][0][0][0].T
-            Y = data['worm']['posture'][0][0][0]['skeleton'][0][0][0][0].T
-            
-            '''   
-            #getting the x and y coordinates:
-            X = np.array(f.get('x'))
-            Y = np.array(f.get('y'))'''
-                
-            indices = [j for j in range(len(X)) if np.sum(np.isnan(X[j]))== 0]
-                        
-            X = X[indices] 
-            Y = Y[indices]
-            
-            #I should probably add a test for frame sparsity as well. At present I'm 
-            #assuming that frame sparsity isn't an issue. 
-            if len(X) > 1000:
+    angle_data = loading_data(directory,sampling_fraction)
+    
+    i = 0
+    while i < len(angle_data):
         
+            
+        #I should probably add a test for frame sparsity as well. At present I'm 
+        #assuming that frame sparsity isn't an issue. 
+        if len(angle_data[i][1]) > 1000:
+            
             #get angles for the skeletons
-                angles, m_a = angle(X,Y)
-                #X, Y = MA2skel(angles, m_a, 1)
+            angles, m_a = angle_data[i]
+            #X, Y = MA2skel(angles, m_a, 1)
                 
-                #initialize Vars and posture_sequence:
-                #Vars = np.zeros(len(X))
-                posture_sequence = ''
-                #angle_err = np.zeros(len(X))
+            #initialize Vars and posture_sequence:
+            #Vars = np.zeros(len(X))
+            posture_sequence = ''
                 
-                for i in range(len(X)):
-                    distances = [np.inf]*num_postures
-                    for j in range(num_postures):
-                        distances[j] = np.linalg.norm(angles[i]-postures[:,j])
-                    val = min(distances)
-                    #angle_err[i] = val
-                    ind = distances.index(val)
-                    #Vars[i] = np.corrcoef(angles[i],postures[:,ind])[0][1]**2
-                    posture_sequence = posture_sequence + ' ' + str(ind)
+            for i in range(len(angles)):
+                distances = [np.inf]*num_postures
+                for j in range(num_postures):
+                    distances[j] = np.linalg.norm(angles[i]-postures[:,j])
+                val = min(distances)
+                #angle_err[i] = val
+                ind = distances.index(val)
+                #Vars[i] = np.corrcoef(angles[i],postures[:,ind])[0][1]**2
+                posture_sequence = posture_sequence + ' ' + str(ind)
                 all_postures.append(posture_sequence)
                 
                 i+=1
                 
-            else:
-                i+=1
+        else:
+            i+=1
             
-        return all_postures
+    return all_postures
