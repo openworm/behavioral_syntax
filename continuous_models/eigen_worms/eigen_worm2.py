7fee2f5c97edfe77b7ec6a90948fca3f75c2142f
Aidan Rocke <aidanrocke@gmail.com>
2016-01-05 18:09:01 +0800

    cont

--- continuous_models/eigen_worms/eigen_worm2.py
+++ continuous_models/eigen_worms/eigen_worm2.py
@@ -9,18 +9,18 @@ import numpy as np
 import seaborn as sns
 
 from behavioral_syntax.utilities import angle_and_skel
-from angle_and_skel import angle, angle2skel
+#from angle_and_skel import angle, angle2skel
 from behavioral_syntax.utilities import loading_data
 
 #from scipy.stats import spearmanr
 
 from scipy.io import loadmat
 
-raw = loadmat('/Users/cyrilrocke/Documents/c_elegans/data/raw_data/N2 on food L_2009_11_19__12_47_47___4___7_features.mat')
+#raw = loadmat('/Users/cyrilrocke/Documents/c_elegans/data/raw_data/N2 on food L_2009_11_19__12_47_47___4___7_features.mat')
 
-postures = loadmat('/Users/cyrilrocke/behavioral_syntax/data/postures.mat')
+#postures = loadmat('/Users/cyrilrocke/behavioral_syntax/data/postures.mat')
 
-pos = postures.get('postures')
+#pos = postures.get('postures')
 
 x,y = loading_data.get_skeletons('/Users/cyrilrocke/Documents/c_elegans/data/raw_data/N2 on food L_2009_11_19__12_47_47___4___7_features.mat')
 
@@ -28,14 +28,14 @@ x,y = loading_data.get_skeletons('/Users/cyrilrocke/Documents/c_elegans/data/raw
 
 #rho, pval = spearmanr(pos)
 
-covariance_matrix = np.cov(pos)
+#covariance_matrix = np.cov(pos)
 
-sns.heatmap(covariance_matrix)
-
+#sns.heatmap(covariance_matrix)
+"""
 eigen, eigenvec = np.linalg.eigh(covariance_matrix)
 
 zog = [np.abs(i) for i in eigen]
-
+"""
 #biggest_eigen = np.sort(mag)
 """
 
@@ -63,12 +63,33 @@ covar = np.cov(np.transpose(angles[0]))
 eigen, eigenvec = np.linalg.eigh(covar)
 
 
-theta = 0
+theta = np.zeros(48)
 
-for i in range(48):
-    theta += eigenvec[i]*eigen[i]
+for i in range(10,48):
+    theta += eigenvec[i]*np.dot(eigenvec[i],angles[0][0])
+    
 
-theta_ = np.multiply(theta,pos[:,0])
+x,y = angle_and_skel.angle2skel(theta,np.mean(theta),1,48)
+
+x2,y2 = angle_and_skel.angle2skel(angles[0][0],np.mean(angles[0][0]),1,48)
+
+sns.plt.plot(x,y)
+
+sns.plt.plot(x2,y2)
+
+#let's look at how good the last 4 angles are on average:
+stat_vec = np.zeros(21004)
+
+for i in range(21004):
+    theta = np.zeros(48)
+
+    for j in range(15,48):
+        theta += eigenvec[j]*np.dot(eigenvec[j],angles[0][0])
+
+    stat_vec[i] = spearmanr(theta,angles[0][i])[0]
+
+
+#theta_ = np.multiply(theta,pos[:,0])
 
 
 def arc_length(x,y):
