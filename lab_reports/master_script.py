d5db162de1de6b242ef3397706ca654219dac5af
Aidan Rocke <aidanrocke@gmail.com>
2016-01-29 01:09:04 +0000

    l4

---
+++ lab_reports/master_script.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 29 00:54:30 2016
+
+@author: aidanrocke
+"""
+
+import os
+
+#run a several ipython notebooks in one go:
+os.system('ipython notebook /Users/cyrilrocke/behavioral_syntax/lab_reports/shabaz.ipynb')
+
