915baae66e9e826c74f21dad7a317dac88166b13
Aidan Rocke <aidanrocke@gmail.com>
2016-02-02 14:34:59 +0000

    changes made

--- lab_reports/master_script.py
+++ lab_reports/master_script.py
@@ -7,6 +7,24 @@ Created on Fri Jan 29 00:54:30 2016
 
 import os
 
+#figure out what type of analysis the user wants to do:
+txt= """Give the number of the type of analysis you want to do:
+        1) one worm species in one environment
+        2) one species in several environments
+        3) different species in similar environments
+        
+        ex. 2 if you want option 2"""
+
+value = input(txt)
+
+
+#ask for where the user wants to save the data and the output lab_reports.
+#the default option is to save the data and lab_reports in the lab_reports 
+#folder
+
+#if int(value) == 2:
+    
+
 #run a several ipython notebooks in one go:
 os.system('ipython notebook /Users/cyrilrocke/behavioral_syntax/lab_reports/shabaz.ipynb')
 
--- models/discrete_models/discretization/posture_distributions.py
+++ models/discrete_models/discretization/posture_distributions.py
@@ -6,9 +6,56 @@ Created on Fri Jan 29 18:38:57 2016
 """
 from behavioral_syntax.utilities.get_features import get_features
 
+from behavioral_syntax.utilities.loading_data import loading_data
+from scipy import io
+
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy.stats import itemfreq
+
+plt.style.use('ggplot')
+
 postures = '/Users/cyrilrocke/Documents/c_elegans/data/postures'
+g = io.loadmat(postures)
+postures = g.get('postures')
 
-data = '/Users/cyrilrocke/Documents/c_elegans/data/chemotaxis/N2Laura_benzaldehyde_L_2013_03_17__15_39_19___2____features.mat'
+data = '/Users/cyrilrocke/Documents/c_elegans/data/chemotaxis/'
 
-pos_seq = get_features(data,)
+pos_seq = get_features(data,postures,0.3,2)
 
+N = len(pos_seq)
+distribution = np.zeros((N,90))    
+
+fig, axes = plt.subplots(ncols=10, nrows=9)
+
+fig.set_size_inches(30, 30)
+ax = axes.ravel()
+
+image_name = 'posture_distributions'
+
+fig.suptitle(image_name,fontsize=40,weight='bold')
+
+for i in range(N):
+    alpha1 = pos_seq[i].split(' ')
+    alpha2 = list(map(int,[i for i in alpha1 if i != '']))
+    z = itemfreq(alpha2)
+    
+    set1 = list(z[:,0])
+    
+    for j in range(90):
+        if set1.count(j) == 0:
+            distribution[i][j] = 0
+        else:
+            distribution[i][j] = z[:,1][set1.index(j)]
+            
+for k in range(90):
+    ax[k].bar(range(N),distribution[:,k])
+    ax[k].set_title(str(k),size='medium',weight='bold',color='steelblue',backgroundcolor=(1,  0.85490196,  0.7254902))
+    
+image_loc = '/Users/cyrilrocke/Documents/c_elegans/data/'
+
+
+if isinstance(image_loc+image_name,str):
+        fig.savefig(image_loc+image_name+'.png',dpi=fig.dpi)
+
+#angles = loading_data(data, 0.2)
